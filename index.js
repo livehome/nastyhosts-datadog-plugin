3d38a0da02474ea28b7553602a50d6a0ebc9388e
Kacper Żuk <kacper.b.zuk@gmail.com>
2016-03-04 10:34:12 +0100

    Add namespace

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const expressStatsD = require('express-statsd');
 
 module.exports = function(app, config, _ipcheck) {
+    app.use((req, res, next) => {
+        req.statsdKey = "nastyhosts";
+        next();
+    });
     app.use(expressStatsD());
 }
